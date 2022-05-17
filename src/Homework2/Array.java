f7de359346b6b60460a9b6d204af58de7c69259f
Vahagn-Meliqyan <105382604+Vahagn-Meliqyan@users.noreply.github.com>
2022-05-17 15:54:43 +0400

    Homework3

--- src/Homework2/Array.java
+++ src/Homework2/Array.java
@@ -3,8 +3,7 @@ package Homework2;
 public class Array {
     public static void main(String[] args)
     {
-        int[] array = {15, 35, 81, 11, 22, 23, 5, 1, 2, 0
-        };
+        int[] array = {15, 35, 81, 11, 22, 23, 5, 1, 2, 0};
  //Տպեք մասիվի բոլոր էլեմենտները
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
@@ -12,57 +11,58 @@ public class Array {
         }
         System.out.println();
 //Տպեք մասիվի ամենամեծ թիվը
-        int largest = 1;
-
+        int largest =1;
         for (int i = 0; i < array.length; i++) {
-            if (array[i] > array[largest]) {
-                largest++;
-                i--;
+            for (int b = 1; b<i; b++) {
+                if (array[i]>array[largest]) {
+                    largest++;
+
+                }
             }
         }
-        for (int i = 0; i < 1; i++) {
-            System.out.println(array[largest] + " ");
-        }
+            System.out.println(array[largest]);
+
 //  Տպեք մասիվի ամենափոքրը թիվը
-        int smallest = 1;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] < array[smallest]) {
-                smallest++;
-                i--;
+            int smallest = 0;
+            for (int i = 0; i < array.length; i++) {
+                for (int r = 0; r < i; r++) {
+                    if (array[i] < array[smallest]) {
+                        smallest++;
+                    }
+                }
+
             }
-        }
-        for (int i = 0; i < 1; i++) {
 
+            System.out.println(array[smallest]);
 
-            System.out.println(array[smallest] + "  ");
-        }
 //Տպեք մասիվի բոլոր զույգ էլեմենտները
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] % 2 == 0) {
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] % 2 == 0) {
 
 
-                System.out.print(array[i] + "  ");
+                    System.out.print(array[i]+ " " );
+                }
             }
-        }
-        System.out.println();
+            System.out.println();
 //Տպեք մասիվի բոլոր կենտ էլեմենտները
 
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] % 2 != 0) {
-                System.out.print(array[i] + "  ");
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] % 2 != 0) {
+                    System.out.print(array[i] + "  ");
+                }
             }
-        }
-        System.out.println();
+            System.out.println();
 //Տպեք զույգերի քանակը
 
 
-        int even = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] % 2 == 0) {
-                even++;
+            int even = 0;
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] % 2 == 0) {
+                    even++;
+                }
+
             }
 
-        }
         System.out.println(even + " ");
  //Տպեք կենտերի քանակը
         int odd = 0;
@@ -76,19 +76,19 @@ public class Array {
  //Տպեք մասիվի բոլոր թվերի միջին թվաբանականը (բոլոր էլեմենտների գումարը / էլեմենտների քանակի վրա)
         int average = 0;
         for (int i = 0; i < array.length; i++) {
-            average = average + array[i];
+            average += array[i];
 
         }
-        double integer = 0;
+        double sum = 0;
         for (int i = 0; i < array.length; i++) {
-            integer = integer + array[i];
+            sum +=array[i];
 
         }
-        System.out.println(integer / array.length);
+        System.out.println(sum / array.length);
 //Տպեք մասիվի էլեմենտների գումարը
         int plus = 0;
         for (int i = 0; i < array.length; i++) {
-            plus = plus + array[i];}
+            plus += array[i];}
         {
                 System.out.print(plus);
             }
